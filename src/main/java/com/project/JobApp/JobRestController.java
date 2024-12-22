bf093b4b2b9eec2ce6dc39dc413be18a3c2585aa
Shubhamore9397 <shubhamore9397@gmail.com>
2024-12-22 10:33:30 +0530

    Day 2 updation

--- src/main/java/com/project/JobApp/JobRestController.java
+++ src/main/java/com/project/JobApp/JobRestController.java
@@ -18,10 +18,12 @@ public class JobRestController {
         return service.getAllJobs();
     }
 
+
     @GetMapping("jobPost/{postId}")
     public JobPost getJob(@PathVariable int postId){
         return service.getJob(postId);
     }
+
 
     @PostMapping("jobPost")
     public JobPost addJob(@RequestBody JobPost jobPost){
@@ -29,16 +31,31 @@ public class JobRestController {
         return service.getJob(jobPost.getPostId());
     }
 
+
     @PutMapping("jobPost")
     public JobPost updateJob(@RequestBody JobPost jobPost){
         service.updateJob(jobPost);
         return service.getJob(jobPost.getPostId());
     }
 
+
     @DeleteMapping("jobPost/{postId}")
     public String deleteJob(@PathVariable int postId)
     {
         service.deleteJob(postId);
         return "Deleted";
+    }
+
+
+    @GetMapping("jobPosts/keyword/{keyword}")
+    public List<JobPost> searchByKeyword(@PathVariable String keyword){
+        return service.search(keyword);
+    }
+
+
+    @GetMapping("load")
+    public String loadData(){
+        service.load();
+        return "Success";
     }
 }
